91d42cc5ad564138f6b66b0217724f68f219abc3
truongtdz <167507674+truongtdz@users.noreply.github.com>
2024-08-06 21:14:13 +0700

    Update InterfaceInputError.java

--- InterfaceInputError.java
+++ InterfaceInputError.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 public class InterfaceInputError {
     // public static void main(String[] args)
     public InterfaceInputError()
