246c33a3e0580dda92637af8da5391b7e21d7c1a
truongtdz <167507674+truongtdz@users.noreply.github.com>
2024-08-06 21:14:36 +0700

    Update InterfaceEnterNameDelete.java

--- InterfaceEnterNameDelete.java
+++ InterfaceEnterNameDelete.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-
 public class InterfaceEnterNameDelete{
     public InterfaceEnterNameDelete(ArrayList<Student> list, MenuProject menu){
         JFrame frame = new JFrame();   
